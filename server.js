1b5367044676bbda522051f578f652dd7fcbcf9b
the-mega-byte <noahledbetter4@gmail.com>
2024-07-07 17:01:43 -0400

    everything aside from the CSS currently works will get that running soon

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const express = require('express');
-const axios = require('axios');
+
+// express app
 const app = express();
-const PORT = process.env.PORT || 8081;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'the-mega-byte' || 'kevincripe';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+// listen for requests
+app.listen(3000);
 
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  const courses = [
+    {title: 'Course 1', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Course 2', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Course 3', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', courses });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/courses/create', (req, res) => {
+  res.render('create', { title: 'Create a new course' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
