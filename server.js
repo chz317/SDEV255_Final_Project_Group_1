a2b5cb638e830942a716b519a21e9faabbfed64f
Kevin Cripe <kevincripe6@gmail.com>
2024-07-10 17:06:39 -0500

    cleanup

--- server.js
+++ server.js
@@ -74,26 +74,6 @@ app.get('/courses/:id', (req, res) => {
     });
 });
 
-// Update a course by its id
-app.put('/courses/:id', async (req, res) => {
-  const course = Course(req.body);
-  await Course.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    .then((course) => {
-      if (!course) {
-        return res.status(404).send();
-      }
-      res.send(course);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-  //Course.updateOne({_id: req.params.id}, req.body).then(response => {
-  //  res.status(200).send(response);
-  //}).catch(err => {
-  //  res.status(500).send(err);
-  //});
-});
-
 // Delete a course by its id
 app.delete('/courses/:id', (req, res) => {
   const id = req.params.id;
