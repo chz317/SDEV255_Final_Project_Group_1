7536992e9cb5f6a66cf369892b2db60353066f6f
Joseph Cheesebourough <joseph.cheeseb@gmail.com>
2024-07-19 21:12:31 -0400

    server update

--- server.js
+++ server.js
@@ -4,25 +4,22 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const morgan = require('morgan');
+const path = require('path');
+
 const courseRoutes = require('./routes/courseRoutes');
 const authRoutes = require('./routes/authRoutes');
 const User = require('./models/user');
 
-const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
-
-
-const app = express();
+const app = express(); 
 
 // Connect to MongoDB
 const dbURI = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@sdev255projectteam1.aw1cgbj.mongodb.net/SDEV255ProjectTeam1`;
 mongoose.connect(dbURI)
-  .then(result => app.listen(3000, () => console.log('Server started on port 3000')))
+  .then(result => app.listen(3000))
   .catch(err => console.log(err));
 
-// Middleware for parsing request bodies
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Session configuration
@@ -36,17 +33,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Register view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Ensure this is after app initialization
+
+// Middleware & static files
+app.use(express.static('public'));
+
 // Configure passport local strategy
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-// Register view engine
-app.set('view engine', 'ejs');
-
-// Middleware & static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
 
 // Define your routes here
 app.get('/', (req, res) => res.redirect('/courses')); // Redirect to courses page
@@ -63,3 +60,5 @@ app.use('/courses', courseRoutes);
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+app.listen(3000, () => console.log('Server started on port 3000'));
